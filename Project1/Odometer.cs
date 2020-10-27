d424ed07b214b29e86c3c4c402e6a85687bff124
RossanaPalma <70126235+RossanaPalma@users.noreply.github.com>
2020-10-26 23:02:45 -0700

    Add project files.

---
+++ Project1/Odometer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1
+{
+    public class Odometer
+    {
+        //Instance Variables
+        private int miles;
+        private double fuelUsed;
+
+        //Constructors
+        public Odometer()
+        {
+            miles = 0;
+            fuelUsed = 15;
+        }
+        public Odometer(int miles, double fuelUsed)
+        {
+            this.miles = miles;
+            this.fuelUsed = fuelUsed;
+        }
+        //Methods
+        public void setMiles(int m)
+        {
+            miles = m;
+        }
+        public int getMiles()
+        {
+            return miles;
+        }
+        public void setfuelUsed(double f)
+        {
+            fuelUsed = f;
+        }
+        public double getFuelUsed()
+        {
+            return fuelUsed;
+        }
+        public void reset()
+        {
+            miles = 0;
+            fuelUsed = 0;
+        }
+        public void Add (int m, double f)
+        {
+            miles += m;
+            fuelUsed += f;
+        }
+        public double mpg()
+        {
+            return ( miles / fuelUsed );
+        }
+        public string toString()
+        {
+            return " " + miles + " Miles driven " + fuelUsed + " fuel used";
+        }
+        public bool equals(Odometer other)
+        {
+           // return (this.miles == other.miles) && (this.fuelUsed == other.fuelUsed);
+            return (this.getMiles() == other.getMiles()) && (this.getFuelUsed() == other.getFuelUsed());
+        }
+    }
+}
---
+++ Project1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Odometer trip1 = new Odometer();
+            trip1.Add(50, 10);
+            Console.WriteLine("fuel used for trip1 is: " + trip1.mpg());
+            Odometer trip2 = new Odometer(50, 10);
+            trip2.Add(100, 20);
+            Console.WriteLine("fuel used for trip2 is: " + trip2.mpg());
+            Console.WriteLine(trip1.toString());
+            Console.WriteLine(trip2.toString());
+            if (trip1.equals(trip1))
+            {
+                Console.WriteLine("The two object are the same");
+            }
+            else
+                Console.Write("The two objects are NOT the same");
+            //toDo : reset
+            //ToDo : test toString();
+            //test every method
+        }
+        public static int add(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
